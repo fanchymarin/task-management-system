211911adabbe82a44fd843877ba3fc776ccdeb7a
Fran Marín <franmp97@outlook.com>
2025-09-06 21:14:14 +0200

    Django REST added

--- src/app/urls.py
+++ src/app/urls.py
@@ -20,6 +20,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('', RedirectView.as_view(url='invoices/', permanent=True)),
+    path('api/auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('invoices/', include('invoices.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
