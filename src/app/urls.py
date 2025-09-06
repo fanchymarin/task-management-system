e6ca79b9211f0807b056a622749d1c3cd81eb120
Fran Marín <franmp97@outlook.com>
2025-09-06 21:49:47 +0200

    /api/ root webpage

--- src/app/urls.py
+++ src/app/urls.py
@@ -14,13 +14,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+
+from django.contrib import admin
 from django.views.generic.base import RedirectView
 
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, login URLs are included for the browsable API.
 urlpatterns = [
-    path('', RedirectView.as_view(url='invoices/', permanent=True)),
-    path('api/auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls)),
+    path('api/auth/', include('rest_framework.urls', namespace='rest_framework')),
+
     path('admin/', admin.site.urls),
     path('invoices/', include('invoices.urls')),
     path("accounts/", include("django.contrib.auth.urls")),
