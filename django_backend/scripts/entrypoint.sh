4fa4338eac0fa2ca88b093b8932dd032687178dc
Fran Marín <franmp97@outlook.com>
2025-09-08 16:43:04 +0200

    AbstractUser implementation removed

--- django_backend/scripts/entrypoint.sh
+++ django_backend/scripts/entrypoint.sh
@@ -7,11 +7,10 @@ python manage.py makemigrations --noinput
 python manage.py migrate
 
 # Create superuser
-python manage.py createsuperuser --noinput --first_name "Admin" --last_name "User"
+python manage.py createsuperuser --noinput
 
 # Load initial data
 python manage.py dbshell < $DUMP_FILE
-
 
 # Start server
 python manage.py runserver 0.0.0.0:8000
