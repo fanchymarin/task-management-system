de7884e26131a695f132eb50bf03c00be0f2c5d8
Fran Marín <franmp97@outlook.com>
2025-09-09 13:08:10 +0200

    added pagination

--- django_backend/apps/users/views.py
+++ django_backend/apps/users/views.py
@@ -4,11 +4,18 @@ from .models import User
 from .serializers import UserSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+
+class UserPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     http_method_names = ['get', 'put', 'head']
+    pagination_class = UserPagination
 
     @action(methods=['get'], detail=False, url_path='me', url_name='me')
     def me(self, request):
